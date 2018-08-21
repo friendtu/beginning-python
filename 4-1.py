ebf07ec180b01bc0be7d788f4e400a7dc83d2632
friendtu <friendtu@gmail.com>
2018-08-21 10:04:12 +0800

    dictionary4-1

---
+++ 4-1.py
@@ -0,0 +1,25 @@
+people={
+    "Alice":{
+        'phone':'1232',
+        'addr':"shanghai"
+    },
+    "Donnie":{
+        'phone':"123232",
+        'addr':"beijin"
+    }
+}
+
+labels={
+    'phone':"phone number",
+    'addr':'address'
+}
+
+name=input("Name: ")
+request=input("Phone(p) or Address(a):")
+if request=='p':
+    key='phone'
+if request=='a':
+    key='addr'
+
+if name in people: 
+    print("{}'s {} is {}".format(name,labels[key],people[name][key]))
