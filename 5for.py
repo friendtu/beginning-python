776c56cf19e5a6fa7def7eaf7442b1396e6d82c9
friendtu <friendtu@gmail.com>
2018-08-23 22:16:37 +0800

    for

---
+++ 5for.py
@@ -0,0 +1,11 @@
+names=['Donnie',"GuaGua"]
+i=0
+while i<len(names):
+    if 'Gua' in names[i]:
+        print ("Hello,{}".format(names[i]))
+    i=i+1
+
+for index,string in enumerate(names):
+    if 'Gua' in string:
+        print ("Hello,%s" % string)
+    
