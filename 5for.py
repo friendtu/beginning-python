3d760179b04b389a54dcfcc02ac363bee078255f
tud <tud@Donnies-MacBook-Air.local>
2019-04-12 11:02:50 +0800

    from mac

--- 5for.py
+++ 5for.py
@@ -4,7 +4,7 @@ while i<len(names):
     if 'Gua' in names[i]:
         print ("Hello,{}".format(names[i]))
     i=i+1
-
+#add a line from macbook
 for index,string in enumerate(names):
     if 'Gua' in string:
         print ("Hello,%s" % string)
